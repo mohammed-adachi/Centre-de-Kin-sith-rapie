2e0d3362dbe3ad6bc3e6329ce57d3ca18e9ba61c
Mohammed adachi <mohammed.adachi@uit.ac.ma>
2025-01-09 17:42:07 +0100

    Add UserDetailsService implementation for user authentication

---
+++ src/main/java/com/kinesitherapie/kinesitherapie/service/serviceUser.java
@@ -0,0 +1,35 @@
+package com.kinesitherapie.kinesitherapie.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.kinesitherapie.kinesitherapie.userrepositry;
+import com.kinesitherapie.kinesitherapie.models.user;
+
+
+@Service
+public class serviceUser implements UserDetailsService  {
+    @Autowired
+    private userrepositry userrepositry;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+      
+     user user = userrepositry.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+            user.getUsername(),
+            user.getPassword(),  // Assurez-vous que c'est le mot de passe encodé
+            new ArrayList<>()
+        );  
+      
+    }
+
+}
