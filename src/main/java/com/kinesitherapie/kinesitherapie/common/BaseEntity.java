b80470577019ddd345f90bb5a7c26b46fb535405
Mohammed adachi <mohammed.adachi@uit.ac.ma>
2025-01-23 21:29:53 +0100

    Ajout de my-angular-app et autres modifications

--- src/main/java/com/kinesitherapie/kinesitherapie/common/BaseEntity.java
+++ src/main/java/com/kinesitherapie/kinesitherapie/common/BaseEntity.java
@@ -16,8 +16,6 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 
 public class BaseEntity {
 
---
+++ src/test/java/com/kinesitherapie/kinesitherapie/KinesitherapieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinesitherapie.kinesitherapie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesitherapieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
